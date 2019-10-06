39cf55f258b55a9f8f05eaef180ac5ded56a0f18
Paweł Słowik <pawel.slowik@zoho.com>
2019-10-07 00:32:27 +0200

    DRY

--- src/Validator/NewDistrictValidator.php
+++ src/Validator/NewDistrictValidator.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Validator;
 
-class NewDistrictValidator implements Validator
+class NewDistrictValidator extends DistrictValidator implements Validator
 {
     protected $validCityIds;
 
@@ -15,11 +15,8 @@ class NewDistrictValidator implements Validator
 
     public function validate(array $data): ValidationResult
     {
-        $result = new ValidationResult();
+        $result = parent::validate($data);
         $this->validateCity($data, $result);
-        $this->validateName($data, $result);
-        $this->validateArea($data, $result);
-        $this->validatePopulation($data, $result);
         return $result;
     }
 
@@ -35,47 +32,5 @@ class NewDistrictValidator implements Validator
             return;
         }
         $result->addValidatedData("city", $city);
-    }
-
-    protected function validateName(array $data, ValidationResult $result): void
-    {
-        if (!array_key_exists("name", $data)) {
-            $result->addError("name");
-            return;
-        }
-        $name = trim(strval($data["name"]));
-        if ($name === "") {
-            $result->addError("name");
-            return;
-        }
-        $result->addValidatedData("name", $name);
-    }
-
-    protected function validateArea(array $data, ValidationResult $result): void
-    {
-        if (!array_key_exists("area", $data)) {
-            $result->addError("area");
-            return;
-        }
-        $area = floatval($data["area"]);
-        if ($area <= 0) {
-            $result->addError("area");
-            return;
-        }
-        $result->addValidatedData("area", $area);
-    }
-
-    protected function validatePopulation(array $data, ValidationResult $result): void
-    {
-        if (!array_key_exists("population", $data)) {
-            $result->addError("population");
-            return;
-        }
-        $population = intval($data["population"]);
-        if ($population <= 0) {
-            $result->addError("population");
-            return;
-        }
-        $result->addValidatedData("population", $population);
     }
 }
