9d880130030771ed21cfde7d2a0f0327a2cdb374
Paweł Słowik <pawel.slowik@zoho.com>
2020-01-12 22:45:47 +0100

    Use an array for setting up dependencies

--- src/dependencies.php
+++ src/dependencies.php
@@ -9,28 +9,34 @@ use Slim\Interfaces\RouteParserInterface;
 use Repository\DistrictRepository;
 
 return function (App $app): void {
-    $container = $app->getContainer();
+    $dependencies = [
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set(Twig::class, function ($container) {
-        return Twig::create(
-            "templates",
-            [
-                "cache" => "/tmp/twig_cache",
-                "auto_reload" => true,
-            ]
-        );
-    });
+        // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+        Twig::class => function ($container) {
+            return Twig::create(
+                "templates",
+                [
+                    "cache" => "/tmp/twig_cache",
+                    "auto_reload" => true,
+                ]
+            );
+        },
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set(RouteParserInterface::class, function ($container) use ($app) {
-        return $app->getRouteCollector()->getRouteParser();
-    });
+        // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+        RouteParserInterface::class => function ($container) use ($app) {
+            return $app->getRouteCollector()->getRouteParser();
+        },
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set(DistrictRepository::class, function ($container) {
-        $entityManagerFactory = require "doctrine-bootstrap.php";
-        $entityManager = $entityManagerFactory();
-        return new DistrictRepository($entityManager);
-    });
+        // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+        DistrictRepository::class => function ($container) {
+            $entityManagerFactory = require "doctrine-bootstrap.php";
+            $entityManager = $entityManagerFactory();
+            return new DistrictRepository($entityManager);
+        },
+
+    ];
+
+    foreach ($dependencies as $dependency => $factory) {
+        $app->getContainer()->set($dependency, $factory);
+    }
 };
