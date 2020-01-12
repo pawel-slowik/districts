bd6fa627963d13d04d47d9fa7df671026fd97c6a
Paweł Słowik <pawel.slowik@zoho.com>
2020-01-12 22:34:20 +0100

    PHP-DI autowiring

--- src/dependencies.php
+++ src/dependencies.php
@@ -4,21 +4,15 @@ declare(strict_types=1);
 
 use Slim\App;
 use Slim\Views\Twig;
+use Slim\Interfaces\RouteParserInterface;
 
 use Repository\DistrictRepository;
-use Controller\ListController;
-use Controller\AddFormController;
-use Controller\AddActionController;
-use Controller\EditFormController;
-use Controller\EditActionController;
-use Controller\RemoveFormController;
-use Controller\RemoveActionController;
 
 return function (App $app): void {
     $container = $app->getContainer();
 
     // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set("view", function ($container) {
+    $container->set(Twig::class, function ($container) {
         $options = [
             "cache" => "/tmp/twig_cache",
             "auto_reload" => true,
@@ -28,13 +22,8 @@ return function (App $app): void {
     });
 
     // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set("route_parser", function ($container) use ($app) {
+    $container->set(RouteParserInterface::class, function ($container) use ($app) {
         return $app->getRouteCollector()->getRouteParser();
-    });
-
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    $container->set("session", function ($container) {
-        return new \SlimSession\Helper();
     });
 
     // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
@@ -44,25 +33,4 @@ return function (App $app): void {
         $repository = new DistrictRepository($entityManager);
         return $repository;
     });
-
-    // all the basic controllers have the same dependencies
-    $basicControllerClasses = [
-        ListController::class,
-        AddFormController::class,
-        AddActionController::class,
-        EditFormController::class,
-        EditActionController::class,
-        RemoveFormController::class,
-        RemoveActionController::class,
-    ];
-    foreach ($basicControllerClasses as $controllerClass) {
-        $container->set($controllerClass, function ($container) use ($controllerClass) {
-            return new $controllerClass(
-                $container->get(DistrictRepository::class),
-                $container->get("session"),
-                $container->get("route_parser"),
-                $container->get("view")
-            );
-        });
-    }
 };
