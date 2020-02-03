52f98ac437dee7c7e3626baa34a7d9ae5b0e8783
Paweł Słowik <pawel.slowik@zoho.com>
2020-02-03 20:33:28 +0100

    Optionally select / filter cities for scraping

--- README.md
+++ README.md
@@ -33,7 +33,6 @@ Start the web interface:
 ## TODO
 
 - flash messages
-- select cities for updating
 - replace the validator with Valitron? zend-validator?
 - automatically generate forms with [zend-form and model annotations](https://docs.zendframework.com/zend-form/quick-start/#using-annotations)?
 - more tests
--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -40,14 +42,25 @@ final class UpdateCommand extends Command
             new KrakowScraper($fetcher, $finder),
         ];
         $this->setName("update");
-        $this->setDescription("Update the districts database with scraped data. Overwrites all changes.");
+        $this->setDescription("Update the districts database with scraped data. Overwrites existing records.");
+        $this->addArgument(
+            "city_names",
+            InputArgument::OPTIONAL | InputArgument::IS_ARRAY,
+            "List of city names to update (default: update all cities)"
+        );
     }
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
+        $inputCityNames = $input->getArgument("city_names");
+        if (!$this->checkCityFilter($inputCityNames)) {
+            throw new InvalidArgumentException("unsupported city filter: " . var_export($inputCityNames, true));
+        }
         foreach ($this->scrapers as $scraper) {
             $cityName = $scraper->getCityName();
+            if (!$this->cityFilterMatches($cityName, $inputCityNames)) {
+                continue;
+            }
             $districts = $scraper->listDistricts();
             $this->updateCity($cityName, $districts, $output);
         }
@@ -80,5 +93,21 @@ final class UpdateCommand extends Command
             $district->setCity($city);
             yield $district;
         }
+    }
+
+    private function cityFilterMatches(string $cityName, array $cityNameFilter): bool
+    {
+        return empty($cityNameFilter) || in_array($cityName, $cityNameFilter, true);
+    }
+
+    private function checkCityFilter(array $cityNameFilter): bool
+    {
+        $supportedCityNames = array_map(
+            function ($scraper) {
+                return $scraper->getCityName();
+            },
+            $this->scrapers
+        );
+        return empty(array_diff($cityNameFilter, $supportedCityNames));
     }
 }
