a90ff5ccee7a01287c79375bd2ecda8b59fcbc55
Paweł Słowik <pawel.slowik@zoho.com>
2020-08-25 11:50:55 +0200

    Add DistrictFilter validation

--- src/Service/DistrictFilter.php
+++ src/Service/DistrictFilter.php
@@ -11,12 +11,22 @@ class DistrictFilter
     public const TYPE_AREA = 3;
     public const TYPE_POPULATION = 4;
 
+    private const VALIDATORS = [
+        self::TYPE_CITY => [self::class, "validateString"],
+        self::TYPE_NAME => [self::class, "validateString"],
+        self::TYPE_AREA => [self::class, "validateRange"],
+        self::TYPE_POPULATION => [self::class, "validateRange"],
+    ];
+
     private $type;
 
     private $value;
 
     public function __construct(int $type, $value)
     {
+        if (!self::validate($type, $value)) {
+            throw new \InvalidArgumentException();
+        }
         $this->type = $type;
         $this->value = $value;
     }
@@ -29,5 +39,32 @@ class DistrictFilter
     public function getValue()
     {
         return $this->value;
+    }
+
+    private static function validate(int $type, $value): bool
+    {
+        if (!array_key_exists($type, self::VALIDATORS)) {
+            return false;
+        }
+        return self::VALIDATORS[$type]($value);
+    }
+
+    private static function validateString($value): bool
+    {
+        return is_string($value) && ($value !== "");
+    }
+
+    private static function validateRange($value): bool
+    {
+        return
+            is_array($value)
+            && (count($value) === 2)
+            && array_key_exists(0, $value)
+            && array_key_exists(1, $value)
+            && (is_float($value[0]) || is_int($value[0]))
+            && (is_float($value[1]) || is_int($value[1]))
+            && ($value[0] >= 0)
+            && ($value[1] >= 0)
+            && ($value[1] >= $value[0]);
     }
 }
--- tests/Service/DistrictFilterTest.php
+++ tests/Service/DistrictFilterTest.php
@@ -19,4 +19,57 @@ class DistrictFilterTest extends TestCase
         $this->assertSame(DistrictFilter::TYPE_CITY, $filter->getType());
         $this->assertSame("test", $filter->getValue());
     }
+
+    /**
+     * @dataProvider validDataProvider
+     */
+    public function testValid(int $type, $value): void
+    {
+        $filter = new DistrictFilter($type, $value);
+        $this->assertInstanceOf(DistrictFilter::class, $filter);
+    }
+
+    public function validDataProvider(): array
+    {
+        return [
+            [DistrictFilter::TYPE_CITY, "foo"],
+            [DistrictFilter::TYPE_NAME, "foo"],
+            [DistrictFilter::TYPE_AREA, [1, 2]],
+            [DistrictFilter::TYPE_AREA, [1.1, 2.2]],
+            [DistrictFilter::TYPE_AREA, [1.1, 2]],
+            [DistrictFilter::TYPE_AREA, [1, 2.2]],
+            [DistrictFilter::TYPE_POPULATION, [1, 2]],
+            [DistrictFilter::TYPE_POPULATION, [1.1, 2.2]],
+            [DistrictFilter::TYPE_POPULATION, [1.1, 2]],
+            [DistrictFilter::TYPE_POPULATION, [1, 2.2]],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function testInvalid(int $type, $value): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new DistrictFilter($type, $value);
+    }
+
+    public function invalidDataProvider(): array
+    {
+        return [
+            "invalid type" => [0, "foo"],
+            "not a string" => [DistrictFilter::TYPE_CITY, 1],
+            "invalid string" => [DistrictFilter::TYPE_CITY, ""],
+            "range not an array" => [DistrictFilter::TYPE_AREA, "1-1"],
+            "range missing element" => [DistrictFilter::TYPE_AREA, [1]],
+            "range extra element" => [DistrictFilter::TYPE_AREA, [1, 2, 3]],
+            "range missing begin" => [DistrictFilter::TYPE_AREA, ["a" => 1, 1 => 2]],
+            "range missing end" => [DistrictFilter::TYPE_AREA, [0 => 1, "b" => 2]],
+            "range begin invalid type" => [DistrictFilter::TYPE_AREA, [0 => "1", 1 => 2]],
+            "range end invalid type" => [DistrictFilter::TYPE_AREA, [0 => 1, 1 => "2"]],
+            "range negative begin" => [DistrictFilter::TYPE_AREA, [0 => -1, 1 => 2]],
+            "range negative end" => [DistrictFilter::TYPE_AREA, [0 => 1, 1 => -2]],
+            "range end lower than begin" => [DistrictFilter::TYPE_AREA, [0 => 2.001, 1 => 2]],
+        ];
+    }
 }
