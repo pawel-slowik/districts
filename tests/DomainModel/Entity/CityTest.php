f9d4dc4d05117473e7d559b27fdd440090c8239d
Paweł Słowik <pawel.slowik@zoho.com>
2021-07-17 11:04:39 +0200

    Remove unnecessary method

--- tests/DomainModel/Entity/CityTest.php
+++ tests/DomainModel/Entity/CityTest.php
@@ -16,35 +16,37 @@ class CityTest extends TestCase
 {
     public function testAddThrowsExceptionOnValidationFailure(): void
     {
-        $city = $this->createTestCity();
+        $city = new City("test");
+
         $this->expectException(ValidationException::class);
+
         $city->addDistrict("test", -123.4, 5678);
     }
 
     public function testUpdateThrowsExceptionOnValidationFailure(): void
     {
-        $city = $this->createTestCity();
+        $city = new City("test");
+
         $this->expectException(ValidationException::class);
+
         $city->updateDistrict(1, "test", -123.4, 5678);
     }
 
     public function testUpdateThrowsExceptionOnUnknownDistrictId(): void
     {
-        $city = $this->createTestCity();
+        $city = new City("test");
+
         $this->expectException(NotFoundException::class);
+
         $city->updateDistrict(1, "test", 123.4, 5678);
     }
 
     public function testRemoveThrowsExceptionOnUnknownDistrictId(): void
     {
-        $city = $this->createTestCity();
-        $this->expectException(NotFoundException::class);
-        $city->removeDistrict(1);
-    }
-
-    private function createTestCity(): City
-    {
         $city = new City("test");
-        return $city;
+
+        $this->expectException(NotFoundException::class);
+
+        $city->removeDistrict(1);
     }
 }
