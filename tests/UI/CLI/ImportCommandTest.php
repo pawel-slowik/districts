b883eb61e1db98575badf8ee95128a0b270e6e82
Paweł Słowik <pawel.slowik@zoho.com>
2021-07-23 13:35:28 +0200

    Mock the CityScraper interface in tests

--- tests/UI/CLI/ImportCommandTest.php
+++ tests/UI/CLI/ImportCommandTest.php
@@ -7,10 +7,7 @@ namespace Districts\Test\UI\CLI;
 use Districts\Application\Importer;
 use Districts\Application\ProgressReporter;
 use Districts\DomainModel\Scraper\CityDTO;
-use Districts\DomainModel\Scraper\Gdansk\CityScraper as GdanskScraper;
-use Districts\DomainModel\Scraper\HtmlFetcher;
-use Districts\DomainModel\Scraper\HtmlFinder;
-use Districts\DomainModel\Scraper\Krakow\CityScraper as KrakowScraper;
+use Districts\DomainModel\Scraper\CityScraper;
 use Districts\UI\CLI\ImportCommand;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -29,9 +26,9 @@ class ImportCommandTest extends TestCase
     private $importer;
 
     /**
-     * @var HtmlFetcher|MockObject
+     * @var MockObject|Scraper
      */
-    private $fetcher;
+    private $scraper;
 
     /**
      * @var ImportCommand
@@ -51,20 +48,15 @@ class ImportCommandTest extends TestCase
     protected function setUp(): void
     {
         $this->importer = $this->createMock(Importer::class);
-        $this->fetcher = $this->createMock(HtmlFetcher::class);
+        $this->scraper = $this->createMock(CityScraper::class);
         $this->input = $this->createMock(InputInterface::class);
         $this->output = $this->createMock(OutputInterface::class);
         // quiet to avoid the need to mock output helpers
         $this->output->method("getVerbosity")->willReturn(OutputInterface::VERBOSITY_QUIET);
 
-        $finder = new HtmlFinder();
-
         $this->command = new ImportCommand(
             $this->importer,
-            [
-                new GdanskScraper($this->fetcher, $finder),
-                new KrakowScraper($this->fetcher, $finder),
-            ]
+            [$this->scraper]
         );
     }
 
@@ -74,6 +66,11 @@ class ImportCommandTest extends TestCase
             ->method("getArgument")
             ->with($this->equalTo("city_names"))
             ->willReturn(["Foo"]);
+
+        $this->scraper
+            ->method("getCityName")
+            ->willReturn("Bar");
+
         $this->expectException(InvalidArgumentException::class);
         $this->command->run($this->input, $this->output);
     }
@@ -83,19 +80,7 @@ class ImportCommandTest extends TestCase
         $this->input
             ->method("getArgument")
             ->with($this->equalTo("city_names"))
-            ->willReturn(["Gdańsk"]);
-
-        $listHtml = file_get_contents(__DIR__ . "/../../DomainModel/Scraper/Gdansk/dzielnice.html");
-        $entryHtml = file_get_contents(__DIR__ . "/../../DomainModel/Scraper/Gdansk/dzielnice_mapa_alert.php?id=16");
-        $fetcherReturnValues = [$listHtml] + array_fill(1, 35, $entryHtml);
-        $this->fetcher
-            ->expects(
-                $this->exactly(count($fetcherReturnValues))
-            )
-            ->method("fetchHtml")
-            ->will(
-                $this->onConsecutiveCalls(...$fetcherReturnValues)
-            );
+            ->willReturn([]);
 
         $this->importer
             ->expects($this->once())
@@ -103,16 +88,6 @@ class ImportCommandTest extends TestCase
             ->with(
                 $this->isInstanceOf(CityDTO::class),
                 $this->isInstanceOf(ProgressReporter::class),
-            )
-            ->will(
-                $this->returnCallback(
-                    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-                    function (CityDTO $cityDTO, ProgressReporter $progressReporter): void {
-                        $this->assertSame("Gdańsk", $cityDTO->getName());
-                        // start the generator
-                        iterator_to_array($cityDTO->listDistricts());
-                    }
-                )
             );
 
         $this->command->run($this->input, $this->output);
