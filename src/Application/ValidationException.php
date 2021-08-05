e078c6e58c39bad869f65a2beee709cc921ab455
Paweł Słowik <pawel.slowik@zoho.com>
2021-08-05 15:23:46 +0200

    Fix uninitialized exception property

--- src/Application/ValidationException.php
+++ src/Application/ValidationException.php
@@ -11,7 +11,7 @@ class ValidationException extends RuntimeException
     /**
      * @var string[]
      */
-    private $errors;
+    private $errors = [];
 
     public function withErrors(array $errors): self
     {
