65ff77f8fdee338e0ca88473c2de4662db66a211
Paweł Słowik <pawel.slowik@zoho.com>
2021-08-10 12:31:49 +0200

    Add test for Redirector

---
+++ tests/UI/Web/RedirectorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Districts\Test\UI\Web;
+
+use Districts\UI\Web\Redirector;
+use Fig\Http\Message\StatusCodeInterface as StatusCode;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\UriInterface;
+use Slim\Interfaces\RouteParserInterface;
+
+/**
+ * @covers \Districts\UI\Web\Redirector
+ */
+class RedirectorTest extends TestCase
+{
+    /**
+     * @var Redirector
+     */
+    private $redirector;
+
+    /**
+     * @var MockObject|RouteParserInterface
+     */
+    private $routeParser;
+
+    protected function setUp(): void
+    {
+        $this->routeParser = $this->createMock(RouteParserInterface::class);
+        $this->redirector = new Redirector($this->routeParser);
+    }
+
+    public function testRedirect(): void
+    {
+        $this->routeParser
+            ->method("fullUrlFor")
+            ->willReturn("foo");
+
+        $response = $this->redirector->redirect($this->createMock(UriInterface::class), "", []);
+
+        $this->assertSame($response->getStatusCode(), StatusCode::STATUS_FOUND);
+        $this->assertSame($response->getHeader("location"), ["foo"]);
+    }
+}
