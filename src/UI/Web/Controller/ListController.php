df0507084c97238603c3bb80972cb78042972694
Paweł Słowik <pawel.slowik@zoho.com>
2021-08-20 13:21:51 +0200

    Move sorting / ordering link creation from a Twig macro to PHP functions

--- src/UI/Web/Controller/ListController.php
+++ src/UI/Web/Controller/ListController.php
@@ -9,6 +9,7 @@ use Districts\UI\Web\Factory\ListDistrictsQueryFactory;
 use Districts\UI\Web\Factory\RoutedPageReferenceFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Interfaces\RouteParserInterface;
 use Slim\Views\Twig as View;
 use SlimSession\Helper as Session;
 
@@ -24,18 +25,22 @@ final class ListController
 
     private $pageReferenceFactory;
 
+    private $routeParser;
+
     public function __construct(
         DistrictService $districtService,
         ListDistrictsQueryFactory $queryFactory,
         Session $session,
         View $view,
-        RoutedPageReferenceFactory $pageReferenceFactory
+        RoutedPageReferenceFactory $pageReferenceFactory,
+        RouteParserInterface $routeParser
     ) {
         $this->districtService = $districtService;
         $this->queryFactory = $queryFactory;
         $this->session = $session;
         $this->view = $view;
         $this->pageReferenceFactory = $pageReferenceFactory;
+        $this->routeParser = $routeParser;
     }
 
     public function __invoke(Request $request, Response $response, array $args): Response
@@ -45,11 +50,16 @@ final class ListController
         $queryParams = $request->getQueryParams();
         $pageCount = $districts->getPageCount();
         $currentPageNumber = is_null($query->getPagination()) ? 1 : $query->getPagination()->getPageNumber();
+        $orderingColumns = [
+            "city",
+            "name",
+            "area",
+            "population",
+        ];
         $templateData = [
             "title" => "List of districts",
             "districts" => $districts->getCurrentPageEntries(),
-            "orderColumn" => $args["column"] ?? null,
-            "orderDirection" => $args["direction"] ?? null,
+            "orderingUrls" => $this->createOrderingUrls("list", $orderingColumns, $args, $queryParams),
             "filterColumn" => $queryParams["filterColumn"] ?? null,
             "filterValue" => $queryParams["filterValue"] ?? null,
             "pagination" => iterator_to_array(
@@ -63,5 +73,54 @@ final class ListController
         ];
         unset($this->session["success.message"]);
         return $this->view->render($response, "list.html", $templateData);
+    }
+
+    private function createOrderingUrls(string $routeName, array $columns, array $args, array $queryParams): array
+    {
+        return array_combine(
+            $columns,
+            array_map(
+                function (string $column) use ($routeName, $args, $queryParams): string {
+                    return $this->createOrderingUrl($routeName, $column, $args, $queryParams);
+                },
+                $columns
+            )
+        );
+    }
+
+    private function createOrderingUrl(string $routeName, string $column, array $routeArgs, array $queryParams): string
+    {
+        return $this->routeParser->urlFor(
+            $routeName,
+            [
+                "column" => $column,
+                "direction" => $this->computeOrderingDirection($column, $routeArgs),
+            ],
+            $this->copyRelevantQueryParams($queryParams)
+        );
+    }
+
+    private function computeOrderingDirection(string $column, array $routeArgs): string
+    {
+        if (array_key_exists("column", $routeArgs)
+            && array_key_exists("direction", $routeArgs)
+            && ($routeArgs["column"] === $column)
+            && ($routeArgs["direction"] === "asc")
+        ) {
+            return "desc";
+        }
+        return "asc";
+    }
+
+    private function copyRelevantQueryParams(array $queryParams): array
+    {
+        $newQueryParams = [];
+        if (array_key_exists("filterColumn", $queryParams)) {
+            $newQueryParams["filterColumn"] = $queryParams["filterColumn"];
+        }
+        if (array_key_exists("filterValue", $queryParams)) {
+            $newQueryParams["filterValue"] = $queryParams["filterValue"];
+        }
+        return $newQueryParams;
     }
 }
