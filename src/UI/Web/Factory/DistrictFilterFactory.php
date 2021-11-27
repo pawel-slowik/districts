38a83c496ced1149471598b0b1931b710f698588
Paweł Słowik <pawel.slowik@zoho.com>
2021-11-27 12:41:04 +0100

    Simplify DistrictFilterFactory internals - be less clever and more obvious

--- src/UI/Web/Factory/DistrictFilterFactory.php
+++ src/UI/Web/Factory/DistrictFilterFactory.php
@@ -8,37 +8,24 @@ use Districts\DomainModel\DistrictFilter;
 
 class DistrictFilterFactory
 {
-    private const PARSERS = [
-        "city" => [
-            "type" => DistrictFilter::TYPE_CITY,
-            "callback" => "strval",
-        ],
-        "name" => [
-            "type" => DistrictFilter::TYPE_NAME,
-            "callback" => "strval",
-        ],
-        "area" => [
-            "type" => DistrictFilter::TYPE_AREA,
-            "callback" => [self::class, "stringToRange"],
-        ],
-        "population" => [
-            "type" => DistrictFilter::TYPE_POPULATION,
-            "callback" => [self::class, "stringToRange"],
-        ],
-    ];
-
     public function createFromRequestInput(?string $column, ?string $value): ?DistrictFilter
     {
         if (is_null($value) || (strval($value) === "")) {
             return null;
         }
-        if (is_null($column) || !array_key_exists($column, self::PARSERS)) {
-            return null;
+
+        switch ($column) {
+            case "city":
+                return new DistrictFilter(DistrictFilter::TYPE_CITY, $value);
+            case "name":
+                return new DistrictFilter(DistrictFilter::TYPE_NAME, $value);
+            case "area":
+                return new DistrictFilter(DistrictFilter::TYPE_AREA, self::stringToRange($value));
+            case "population":
+                return new DistrictFilter(DistrictFilter::TYPE_POPULATION, self::stringToRange($value));
         }
-        return new DistrictFilter(
-            self::PARSERS[$column]["type"],
-            self::PARSERS[$column]["callback"]($value),
-        );
+
+        return null;
     }
 
     private static function stringToRange(string $input): array
