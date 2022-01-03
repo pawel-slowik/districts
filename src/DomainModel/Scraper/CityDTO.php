f0f8afafc4d0ee272d16dae93fe7a7f241c4d4df
Paweł Słowik <pawel.slowik@zoho.com>
2022-01-03 12:56:35 +0100

    Constructor property promotion in scraper DTOs

--- src/DomainModel/Scraper/CityDTO.php
+++ src/DomainModel/Scraper/CityDTO.php
@@ -6,14 +6,10 @@ namespace Districts\DomainModel\Scraper;
 
 class CityDTO
 {
-    private string $name;
-
-    private iterable $districts;
-
-    public function __construct(string $name, iterable $districts)
-    {
-        $this->name = $name;
-        $this->districts = $districts;
+    public function __construct(
+        private string $name,
+        private iterable $districts,
+    ) {
     }
 
     public function getName(): string
--- src/DomainModel/Scraper/DistrictDTO.php
+++ src/DomainModel/Scraper/DistrictDTO.php
@@ -6,17 +6,11 @@ namespace Districts\DomainModel\Scraper;
 
 class DistrictDTO
 {
-    private string $name;
-
-    private float $area;
-
-    private int $population;
-
-    public function __construct(string $name, float $area, int $population)
-    {
-        $this->name = $name;
-        $this->area = $area;
-        $this->population = $population;
+    public function __construct(
+        private string $name,
+        private float $area,
+        private int $population,
+    ) {
     }
 
     public function getName(): string
