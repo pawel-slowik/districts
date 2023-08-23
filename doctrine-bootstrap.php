46163e918484c380c32613488aa43e43c5f7811e
Paweł Słowik <pawel.slowik@zoho.com>
2023-08-23 18:49:14 +0200

    Replace custom cache implementation with symfony/cache

--- doctrine-bootstrap.php
+++ doctrine-bootstrap.php
@@ -2,18 +2,12 @@
 
 declare(strict_types=1);
 
-use Cache\Adapter\PHPArray\ArrayCachePool;
-use Districts\Editor\Infrastructure\DoctrinePsrCache;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\Driver\AttributeDriver;
-use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\ORMSetup;
 
 return function (): EntityManager {
-    $metadataConfig = Setup::createConfiguration(
-        true, // isDevMode
-        null, // proxyDir
-        new DoctrinePsrCache(new ArrayCachePool()), // cache
-    );
+    $metadataConfig = ORMSetup::createConfiguration(true);
     $metadataConfig->setMetadataDriverImpl(new AttributeDriver([__DIR__ . "/src"]));
     $connectionConfig = ["url" => getenv("DB_URL")];
     $entityManager = EntityManager::create($connectionConfig, $metadataConfig);
