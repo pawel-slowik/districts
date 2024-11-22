72559feea02e7a3cca845ff9dcadb35504acc447
Paweł Słowik <pawel.slowik@zoho.com>
2024-11-22 13:26:03 +0100

    Replace ImportCommand instantiation with DI

--- dependencies/cli.php
+++ dependencies/cli.php
@@ -2,12 +2,24 @@
 
 declare(strict_types=1);
 
+use Districts\Scraper\Domain\Gdansk\CityScraper as GdanskScraper;
 use Districts\Scraper\Domain\HtmlFetcher;
+use Districts\Scraper\Domain\Krakow\CityScraper as KrakowScraper;
 use Districts\Scraper\Infrastructure\GuzzleHtmlFetcher;
+use Districts\Scraper\UI\ImportCommand;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 
+use function DI\autowire;
+use function DI\get;
+
 return [
+    ImportCommand::class => autowire()->constructor(
+        scrapers: [
+            get(GdanskScraper::class),
+            get(KrakowScraper::class),
+        ],
+    ),
     HtmlFetcher::class => static fn ($container) => $container->get(GuzzleHtmlFetcher::class),
     ClientInterface::class => static fn ($container) => $container->get(Client::class),
     // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
